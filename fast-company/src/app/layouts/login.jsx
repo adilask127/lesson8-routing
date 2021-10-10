bbc01c01994c98bc82a7739a6bfb12f6a21e7d3c
Adilask127 <adilask@gmail.com>
2021-10-10 15:29:04 +0600

    Initial commit

---
+++ fast-company/src/app/layouts/login.jsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from "react";
+import TextField from "../components/textField";
+import { validator } from "../utils/validator";
+
+const Login = () => {
+    const [data, setData] = useState({ email: "", password: "" });
+    const [errors, setErrors] = useState({});
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+        // setEmail(e.target.value);
+        // console.log(e.target.name);
+    };
+    const validatorConfig = {
+        email: {
+            isRequired: {
+                message: "Электронная почта обязательна для заполнения"
+            },
+            isEmail: {
+                message: "Email введен некорректно"
+            }
+        },
+        password: {
+            isRequired: {
+                message: "Пароль обязательна для заполненияЫ"
+            },
+            isCapitalSymbol: {
+                message: "Пароль должен содержать хофтя бы одну заглавную букву"
+            },
+            isContainDigit: {
+                message: "Пароль должен содержать хофтя бы одно число"
+            },
+            min: {
+                message: "Пароль должен содержать миниму из 8 символов",
+                value: 8
+            }
+        }
+    };
+    useEffect(() => {
+        validate();
+    }, [data]);
+    const validate = () => {
+        const errors = validator(data, validatorConfig);
+        // for (const fieldName in data) {
+        //     if (data[fieldName].trim() === "") {
+        //         errors[fieldName] = `${fieldName} обязательно для заполнения`;
+        //     }
+        // }
+
+        setErrors(errors);
+        return Object.keys(errors).length === 0;
+    };
+    const isValid = Object.keys(errors).length === 0;
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const isValid = validate();
+        if (!isValid) return;
+        console.log(e);
+    };
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 offset-md-3 shadow p-4">
+                    <h3>Login</h3>
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            label="Электронная почта"
+                            name="email"
+                            value={data.email}
+                            onChange={handleChange}
+                            error={errors.email}
+                        />
+                        <TextField
+                            label="Пароль"
+                            type="password"
+                            name="password"
+                            value={data.password}
+                            onChange={handleChange}
+                            error={errors.password}
+                        />
+                        <button
+                            type="submit"
+                            disabled={!isValid}
+                            className="btn btn-primary w-100 mx-auto"
+                        >
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
